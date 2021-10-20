c77211e727886ec20466c07f3724fd8d532f8391
lucanchling <luc.anchling@cpe.fr>
2021-10-20 08:34:40 +0200

    TP5 - First Commit

---
+++ TP5/src/repertoire.c
@@ -0,0 +1,8 @@
+/* Fichier: repertoire.c
+* permet de parcourir le dossier et d'afficher les noms des fichiers
+* auteur: Luc Anchling
+*/
+
+void lire_dossier(char *nom_dossier) {
+    int 
+}
---
+++ TP5/src/repertoire.h
@@ -0,0 +1,6 @@
+/* Fichier: repertoire.h
+* mise en place de la signature des fonctions de repertoire.h
+* auteur: Luc Anchling
+*/
+
+void lire_dossier(char *)
